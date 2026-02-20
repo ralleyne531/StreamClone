7d68026b33d7d54aec1fdfc3e397e417bd61ef03
ralleyne531 <ralleyne531@gmail.com>
2026-02-20 16:44:14 -0500

    engineering stream ui

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,10 @@ import './App.css';
 function App() {
 
   return (
+  <>
     <header>
         <li><ul className="nav">
-                <li>icon</li>
+                <li><i class="fa-regular fa-file-image"></i></li>
                 <li>following</li>
                 <li>browse</li>
                 <li><i class="fa-solid fa-caret-down"></i></li>
@@ -20,6 +21,16 @@ function App() {
         </div>
         </li>
     </header>
+    <main>
+        <div className="sidebar">
+            LOREM
+        </div>
+        <div className="container"><video controls src="src/assets/video/black_screen.mp4"></video></div>
+        <div className="chat">
+            IPSUM
+        </div>
+    </main>
+  </>
   )
 }
 
